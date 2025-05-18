fddf28aac5eca53a5cf8d86ce9f50380682c2ca0
gerardoluisbv <gerardoluisbv@gmail.com>
2025-05-18 17:34:45 -0500

    validate labels in Billboard Component and add name in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,8 @@ const Footer = () => {
     <footer className="bg-white border-t">
         <div className="mx-auto py-10">
             <p className="text-center text-xs text-black">
-                &copy; 2023 Store Name, Inc All rights reserved.
+                &copy; 2025 Store Name, Inc All rights reserved. <br/>
+                Developed by <a href="https://www.gerardoluisbv.com"> gerardoluisbv  </a>
             </p>
         </div>
     </footer>
